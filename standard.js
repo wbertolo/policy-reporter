4b8257295e4c8e0bb39ce07ddbfdc2a5d579c528
Will <willb.can@gmail.com>
2024-11-26 18:34:03 -0500

    updates

--- standard.js
+++ standard.js
@@ -25,6 +25,8 @@ function modThree(input) {
 
 }
 
+module.exports = modThree;
+
 console.log('result of "110" is', modThree('110')); // Case 1 from the Standard Exercise
 console.log('result of "1010" is', modThree('1010')); // Case 2 from the Standard Exercise
 console.log('result of "1101" is', modThree('1101')); // Case 1 of initial example - Result should be 1
---
+++ standard.test.js
@@ -0,0 +1,43 @@
+const modThree = require('./standard'); // Use CommonJS require
+
+describe('modThree function tests', () => {
+  test('modThree should return 0 when input is 0', () => {
+    expect(modThree(0)).toBe(0);
+  });
+
+  test('modThree should return 1 when input is 1', () => {
+    expect(modThree(1)).toBe(1);
+  });
+
+  test('modThree should return 0 for input 10', () => {
+    expect(modThree(10)).toBe(2);
+  });
+
+  test('modThree should return 2 for input 11', () => {
+    expect(modThree(11)).toBe(0);
+  });
+
+  test('modThree should return 1 for input 100', () => {
+    expect(modThree(100)).toBe(1);
+  });
+
+  test('modThree should return 2 for input 101', () => {
+    expect(modThree(101)).toBe(2);
+  });
+
+  test('modThree should return 0 for input 110', () => {
+    expect(modThree(110)).toBe(0);
+  });
+
+  test('modThree should return 1 for input 111', () => {
+    expect(modThree(111)).toBe(1);
+  });
+
+  test('modThree should return 1 for input 10001', () => {
+    expect(modThree(10001)).toBe(2);
+  });
+
+  test('modThree should return 0 for input 10010', () => {
+    expect(modThree(10010)).toBe(0);
+  });
+});
